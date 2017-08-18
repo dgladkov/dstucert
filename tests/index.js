463d791a2167051a425102484b190976c8c942cd
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2017-08-18 18:21:24 +0300

    Better testing

--- tests/index.js
+++ tests/index.js
@@ -2,19 +2,25 @@ var fs = require('fs');
 var test = require('tape');
 var dstucert = require('..');
 
+var obj = require('./Certificate.js');
 var cer = fs.readFileSync(__dirname + '/Certificate.cer');
-var obj = JSON.parse(fs.readFileSync(__dirname + '/Certificate.json'));
-var buf = new Buffer(10);
-buf.fill(0);
 
-test('certificate decoding', function (t) {
+test('certificate decoding', function(t) {
   t.plan(1);
   var result = JSON.stringify(dstucert.decode(cer));
   var expected = JSON.stringify(obj);
   t.equal(result, expected);
 });
 
-test('certificate ping-pong encoding', function (t) {
+test('certificate encoding', function(t) {
+  t.plan(1);
+  var result = JSON.stringify(dstucert.decode(dstucert.encode(obj)));
+  var expected = JSON.stringify(obj);
+  t.equal(result, expected);
+});
+
+
+test('certificate ping-pong encoding', function(t) {
   t.plan(1);
   var result = JSON.stringify(dstucert.decode(dstucert.encode(dstucert.decode(cer))));
   var expected = JSON.stringify(obj);
